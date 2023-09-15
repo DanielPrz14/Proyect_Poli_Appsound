c6d9685de4afddf0e982e707100b46fdb05a6172
DanielPrz14 <145087087+DanielPrz14@users.noreply.github.com>
2023-09-14 22:32:38 -0500

    PlaylistDto.java

--- PlaylistDto.java
+++ PlaylistDto.java
@@ -1,39 +1,48 @@
-
 package com.mycompany.dto;
 
 import java.util.ArrayList;
 
 public class PlaylistDto {
     private String nombrePlaylist;
-    private ArrayList <Cancion> canciones; 
+    private ArrayList<Cancion> canciones;
 
+    // Constructor de la clase PlaylistDto
     public PlaylistDto(String nombrePlaylist) {
         this.nombrePlaylist = nombrePlaylist;
         this.canciones = new ArrayList<>();
     }
 
+    // Getter para obtener el nombre de la lista de reproducción
     public String getNombrePlaylist() {
         return nombrePlaylist;
     }
 
+    // Setter para establecer el nombre de la lista de reproducción
     public void setNombrePlaylist(String nombrePlaylist) {
         this.nombrePlaylist = nombrePlaylist;
     }
 
+    // Getter para obtener la lista de canciones de la lista de reproducción
     public ArrayList<Cancion> getCanciones() {
         return canciones;
     }
 
+    // Método para verificar si una canción ya está en la lista de reproducción
     public Boolean verificarCancion(Cancion cancion) {
-        return !(this.canciones.indexOf(cancion) >-1);
+        // Comprueba si la canción está en la lista de reproducción
+        return !(this.canciones.indexOf(cancion) > -1);
     }
-    
-    public void agregarCancion(Cancion cancion){
-        if(verificarCancion(cancion)){
+
+    // Método para agregar una canción a la lista de reproducción
+    public void agregarCancion(Cancion cancion) {
+        // Verifica si la canción no está en la lista antes de agregarla
+        if (verificarCancion(cancion)) {
             this.canciones.add(cancion);
         }
     }
-    public void eliminarCancion(Cancion cancion){
-      this.canciones.remove(cancion);
-      }
+
+    // Método para eliminar una canción de la lista de reproducción
+    public void eliminarCancion(Cancion cancion) {
+        this.canciones.remove(cancion);
+    }
 }
