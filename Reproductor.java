f3a5216bc6aa5058368726f7391dc964b1638301
DanielPrz14 <145087087+DanielPrz14@users.noreply.github.com>
2023-09-14 22:38:33 -0500

    Reproductor.java

--- Reproductor.java
+++ Reproductor.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.logica;
 
 import com.mycompany.dto.Cancion;
@@ -11,53 +10,44 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Reproductor {
-    //la clase reproductor tiene que tener 3 metodos
-    private ArrayList<Cancion> galeria = Precarga.initBodegaCanciones();
-    private ArrayList<PlaylistDto> playlist = new ArrayList<>();
-       
-    public void play(Cancion cancion){
+    // La clase Reproductor mantiene una galería de canciones predefinidas y listas de reproducción
+
+    private ArrayList<Cancion> galeria = Precarga.initBodegaCanciones(); // Inicializa la galería de canciones con datos predefinidos
+    private ArrayList<PlaylistDto> playlist = new ArrayList<>(); // Lista de listas de reproducción
+
+    // Método para reproducir una canción
+    public void play(Cancion cancion) {
         try {
+            // Abre la URL de la canción en el reproductor predeterminado
             Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + cancion.getUrl());
         } catch (IOException ex) {
             Logger.getLogger(Reproductor.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
+    // Getter para obtener la galería de canciones
     public ArrayList<Cancion> getGaleria() {
         return galeria;
     }
 
+    // Getter para obtener la lista de listas de reproducción
     public ArrayList<PlaylistDto> getPlaylist() {
         return playlist;
     }
-    
-  //  public void reproducirPlay(PlaylistDto playlist){
-    //    reproducirCancion(playlist, 0);
-    //}
-    
-//    private void reproducirCancion(PlaylistDto playlist , Integer index){
-  //      play( playlist.getCanciones().get(index) );
-    //    new Timer().schedule(new TimerTask() {
-      //      @Override
-        //    public void run() {
-          //      if (playlist.getCanciones().size() < (index+1)){
-            //        reproducirCancion(playlist, (index+1));
-              //  }
-            //}
-        //},( playlist.getCanciones().get(index).getDuracion() * 1000 ));
-    //}
-    
-    public ArrayList<Cancion> filtradoPlay(ArrayList<Cancion> playListExistentes){
+
+    // Método para filtrar canciones que no están en una lista de reproducción
+    public ArrayList<Cancion> filtradoPlay(ArrayList<Cancion> playListExistentes) {
         ArrayList<Cancion> filtradoGaleria = new ArrayList<>();
-        galeria.forEach( cancion -> {
-            if( !playListExistentes.contains(cancion) ){
+        galeria.forEach(cancion -> {
+            if (!playListExistentes.contains(cancion)) {
                 filtradoGaleria.add(cancion);
             }
         });
         return filtradoGaleria;
     }
-    
-    public void crearPlayist(String nombre){
-        playlist.add( new PlaylistDto(nombre) );
+
+    // Método para crear una nueva lista de reproducción
+    public void crearPlayist(String nombre) {
+        playlist.add(new PlaylistDto(nombre));
     }
 }
